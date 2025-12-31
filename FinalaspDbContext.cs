7ca06cdaccd74137ab38fda232272c3c625e0a2b
243603-glitch <243603@students.au.edu.pk>
2025-12-31 08:05:27 +0500

    Add files via upload

---
+++ FinalaspDbContext.cs
@@ -0,0 +1,14 @@
+﻿using FInalasp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FInalasp
+{
+    public class FinalaspDbContext : DbContext
+    {
+        public FinalaspDbContext(DbContextOptions<FinalaspDbContext> option) : base(option)
+        {
+        }
+        public DbSet<student> students { get; set; }
+
+    }
+}
